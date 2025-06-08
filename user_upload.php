f7877003524d20813c3daf9a14880d9078e514ab
John Ranel Dimaculangan <johnranel.dimaculangan@gmail.com>
2025-06-08 20:39:07 +1000

    Added the following functionalities (connectMySql, createMySqlDatabase, and createMySqlDatabaseUserTable) for the command --created_table. Also, option checking in mySqlParametersCheck for the -u, -p and -h commands, then check if the database already exists in selectMySqlDatabaseIfExists. Finally, ensured that the function mentioned does proper error handling.

--- user_upload.php
+++ user_upload.php
@@ -1,11 +1,11 @@
 <?php
     $options = getopt("u:p:h:", ["file:", "create_table", "dry_run", "help"]);
 
-    if(count(options) <= 0)
+    if(count($options) <= 0)
         exit("**\nPlease enter your command line options/directives.\nUse --help command to view the options/directives.\n**\n");
 
     if(isset($options["create_table"]))
-        createTable();
+        createTable($options);
 
     if(isset($options["file"]))
         iterateUsersCsv();
@@ -13,12 +13,76 @@
     if(isset($options["help"]))
         showOptionsOrDirectives();
 
-    function createTable() {
-        // TO DO:
-        // 1. Connect database
-        // 2. Create database
-        // 3. Create users table
+    function createTable($options) {
+        echo "**\nProcessing creation of database and user table...\n";
+        $mysql_parameters_arr = mySqlParametersCheck($options);
+        $db_conn = connectMySql($mysql_parameters_arr);
+        $db_selected = selectMySqlDatabaseIfExists($db_conn);
+
+        if(!$db_selected) {
+            createMySqlDatabase($db_conn);
+            createMySqlDatabaseUserTable($db_conn);
+        }
+
+        echo "Database selected.\n";
+        createMySqlDatabaseUserTable($db_conn);
+
         exit("connect and create database, users table functions execute.\n");
+    }
+
+    function mySqlParametersCheck($options) {
+        $host = $options["h"] ?? "";
+        $username = $options["u"] ?? "";
+        $password = $options["p"] ?? "";
+
+        if(!$host || !$username || !$password)
+            exit("The -u (username) -p (password) -h (host) for MySQL are required to run this command.\n**\n");
+        
+        return compact("host", "username", "password");
+    }
+
+    function connectMySql($mysql_parameters_arr) {
+        try {
+            return @mysqli_connect($mysql_parameters_arr["host"], $mysql_parameters_arr["username"], $mysql_parameters_arr["password"]);
+        } catch(Exception $e) {
+            exit("Please check if host, username, and password is correct.\n**\n");
+        }
+    }
+
+    function selectMySqlDatabaseIfExists($db_conn, $database_name = "catalyst") {
+        $database_exists = $db_conn->query("SHOW DATABASES LIKE '$database_name'");
+        if(mysqli_num_rows($database_exists) > 0)
+            return mysqli_select_db($db_conn, $database_name);
+        return false;
+    }
+
+    function createMySqlDatabase($db_conn, $database_name = "catalyst") {
+        $sql_query = "CREATE DATABASE $database_name";
+        if($db_conn->query($sql_query) === TRUE) {
+            selectMySqlDatabaseIfExists($db_conn);
+            echo "Database created successfully.\n";
+        } else {
+            exit("Error occured during creation of the database.\n**\n");
+        }
+    }
+
+    function createMySqlDatabaseUserTable($db_conn) {
+        $table_exists = $db_conn->query("SHOW TABLES LIKE 'users'");
+        if(mysqli_num_rows($table_exists) <=0) {
+            $sql = "CREATE TABLE users (
+                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                surname VARCHAR(100) NOT NULL,
+                email VARCHAR(150) UNIQUE NOT NULL
+            );";
+
+            if($db_conn->query($sql) === TRUE) {
+                exit("Users table created successfully. \n**\n");
+            } else {
+                exit("Error occured during creation of users table. \n**\n");
+            }
+        }
+        exit("Users table already exists.\n**\n");
     }
 
     function iterateUsersCsv() {
